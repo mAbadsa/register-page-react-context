5297605f24191d4b429c0bdac7369351f91d3125
Mu'hammad Alabadsa <42316204+mAbadsa@users.noreply.github.com>
2020-05-01 06:37:02 +0300

    Consuming A Context

--- src/PageContent.js
+++ src/PageContent.js
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
+import { ThemeContext } from "./contexts/ThemeContext";
 
 class PageContent extends Component {
+  static contextType = ThemeContext;
   render() {
+    const { isDarkMode } = this.context;
     const styles = {
-      backgroundColor: "#444",
+      backgroundColor: isDarkMode ? "#333" : "#f5f5f5",
       width: "100%",
       height: "100vw",
     };
